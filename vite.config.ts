226a7d8a6112c5d4817aa25b18211c508a2113d6
Việt Huỳnh <vietthedev@gmail.com>
2026-06-19 08:55:38 +0000

    use cloudflare vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { cloudflare } from "@cloudflare/vite-plugin";
 import { fresh } from "@fresh/plugin-vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
@@ -5,6 +6,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [
     fresh(),
+    cloudflare(),
     tailwindcss(),
   ],
 });
