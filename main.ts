e6de2cd010083e85d2f95ccdfb77bb215ec7b65f
Việt Huỳnh <vietthedev@gmail.com>
2026-07-05 21:23:14 +0000

    feat: handle debug

--- main.ts
+++ main.ts
@@ -5,4 +5,11 @@ import { State } from "@/utils.ts";
 export const app = new App<State>()
   .use(staticFiles())
   .use(trailingSlashes("never"))
-  .fsRoutes();
+  .fsRoutes()
+  .onError("*", (ctx) => {
+    console.log(`Error: ${ctx.error}`);
+    return new Response("Oops!", { status: 500 });
+  })
+  .get("/thrower", () => {
+    throw new Error("fail");
+  });
